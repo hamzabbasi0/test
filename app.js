2b1fbe3663fd26441826eb2c6829b9b9b07bfcac
hamzabbasi0 <105022437+hamzabbasi0@users.noreply.github.com>
2022-06-02 10:25:27 -0700

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
-res.send('Testing Node JS Application');
+res.send('Testing another app');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
